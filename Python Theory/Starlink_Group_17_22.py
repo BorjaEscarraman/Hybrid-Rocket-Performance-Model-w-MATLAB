5ed6daaf231462261dab2be0c379c751d16e8d62
Borja Manuel Fernandez Escarraman <171446222+BorjaEscarraman@users.noreply.github.com>
2026-04-19 09:13:28 -0400

    4-19-26  Starlink_Group_17_22.py
    Enhanced Rocket Science Theory, added loops, stronger variables and Improved the logic of the Ground Phase.

--- Python Theory/Starlink_Group_17_22.py
+++ Python Theory/Starlink_Group_17_22.py
@@ -1,4 +1,4 @@
-# Following through the all 27 Phases of Group 17, Launch 22nd in the Starlink Payloads sent w/ Falcon 9.
+# Following through all 27 Phases of Group 17, Launch 22nd of the Starlink Payloads sent w/ Falcon 9.
 
 # Source: all the checkpoints within the Falcon 9 Flight, 4/18/26, obtained through Next SpaceFlight.
 
@@ -13,7 +13,7 @@
 # Fairing = protective nose shell for payload
 
 # Ground Phase [#1] - Fill the rocket, cool it, wake it up.
-# 1 - LD Verifies Go for Propellant Load.
+# 1 - LD Verifies Go for Propellant Load. ✓
 # The Launch director gives the 'action call' to start filling the Tanks wih Propellant [LOX & RP-1].
 Robert_Plant_verf1 = True # Approves. # Disclaimer, LD's name is up to my discretion, NOT LEGIT. Led Zepellin Rocks...
 
@@ -26,21 +26,64 @@ else:
 
 # 2 - Propellant Load begins.
 
+# Define Propellant & Falcon 9 mass to check for successful loading.
 LOX = 9000 #kg, Oxidizer.
 RP_1 = 3500 #kg, Fuel.
 Prop = LOX + RP_1 # Total mass in kg.
-# Original is more like Falcon_wet_m = 549_060 # kg... includes both stages [dry mass, payload not included] and propellant [wet mass]. 
-# To check that the Rocket is being loaded with my Propellant Mix, I'll assume my own Falcon 9 Dry Mass.
-Falcon_dry_m = 3000
-Falcon_wet_m = 3000
-# Note. commas can't be used as they create tuples [ordered / immutable list.] Instead, I underscored it for readability. Is the same as 549060.
+# Original is more like Falcon_wet_m = 549_060 # kg... includes both stages [dry mass, payload not included] and propellant [wet mass].
+# Note. commas can't be used as they create tuples [ordered / immutable list.] Instead, I underscored it for readability. Is the same as 549060. 
+# For simplicity, I'll assume my own Falcon 9 Dry Mass.
+Falcon_dry_m = 3000 # Reference point to Start Loading mass Change.
 
-Loading = True
-Full_capacity = False
+#Define boolean based variables of the plumbing system that allows Propellant loading [True if Opened / False if Closed].
+Pumps = True
+Valves = True
+Throttles = True
+#Plumbing_op = Pumps + Valves + Throttles # How could I make this variable work as a collector of all variables in a plumbing system
+# But also work as a bool, in order to close it and open it as the loop [Line 58] comes to an end?
 
-while Loading and Full_capacity == False :
-    print("Loading... Show increase in %...")
-    Loading += 1 # Show increase in %'s until it reaches maximum capacity.
+if Pumps and Valves and Throttles:
+    print ("Path cleared to load!")
+
+else:
+    print ("Abort Propellant loading")
+
+Loading = 0 # Falcon_dry_m starting point... Should I just let this be the dry mass?
+Full_capacity = 15_500 # kg /s... AKA Falcon_wet_m.
+
+import random
+
+percent = 0
+
+while percent < 95 and Loading < Full_capacity:
+    step = random.randint(3,15)
+    percent += step
+    print("Loading %", percent) 
+    print("Completed", percent) # How could I make it stop once % 100 in a print?
+    Loading += step
+
+    if percent > 100 and Loading == Full_capacity:
+        percent = 100
+        #Plumbing_op = False
+        Pumps = False
+        Valves = False
+        Throttles = False
+        print("Falcon 9 Fully Loaded with Propellant!")
+    
+    
+#for Loading in range(3000, Full_capacity): # What kind of step should I do to show exponential growth to achieve a natural loading sequence, instead of a linear 1% , 2%, 3%...
+    #print("Loading Initialized Show increase in %...")
+    #Loading += 300 # Show delta % in increments until it reaches Full capacity. # Propellant variable, must be added in delay. Let the load increase exponentially.
+
+    # if Loading < Full_capacity:
+    #     print ("Loading")
+
+    # elif Loading > Full_capacity:
+    #     print("EXCESS ALERT!")
+
+    # elif Loading == Full_capacity:
+    #     print("Loading Completed")
+    #     print("Proceed to Check 3")
 
 
 
