f5d66f0fc5be4748ad79ffe745563fe8bc66e796
Borja Manuel Fernandez Escarraman <171446222+BorjaEscarraman@users.noreply.github.com>
2026-04-20 23:33:05 -0400

    Update Starlink_Group_17_22.py

--- Python Theory/Starlink_Group_17_22.py
+++ Python Theory/Starlink_Group_17_22.py
@@ -1,3 +1,6 @@
+from datetime import datetime # Show updated datetime on every run.
+print(datetime.now())
+
 # Following through all 27 Phases of Group 17, Launch 22nd of the Starlink Payloads sent w/ Falcon 9.
 
 # Source: all the checkpoints within the Falcon 9 Flight, 4/18/26, obtained through Next SpaceFlight.
@@ -32,10 +35,10 @@ RP_1 = 3500 #kg, Fuel.
 Prop = LOX + RP_1 # Total mass in kg.
 # Original is more like Falcon_wet_m = 549_060 # kg... includes both stages [dry mass, payload not included] and propellant [wet mass].
 # Note. commas can't be used as they create tuples [ordered / immutable list.] Instead, I underscored it for readability. Is the same as 549060. 
-# For simplicity, I'll assume my own Falcon 9 Dry Mass.
+# For simplicity, I'll assume my own Falcon 9's Dry Mass.
 Falcon_dry_m = 3000 # Reference point to Start Loading mass Change.
 
-#Define boolean based variables of the plumbing system that allows Propellant loading [True if Opened / False if Closed].
+#Define boolean based variables of the plumbing system to allow Propellant loading [True if Opened / False if Closed].
 Pumps = True
 Valves = True
 Throttles = True
@@ -55,22 +58,21 @@ import random
 
 percent = 0
 
-while percent < 95 and Loading < Full_capacity:
-    step = random.randint(3,15)
+while percent < 90 and Loading < Full_capacity:
+    step = random.randint(3,15) # What is this doing?
     percent += step
     print("Loading %", percent) 
-    print("Completed", percent) # How could I make it stop once % 100 in a print?
-    Loading += step
+#    print("Completed", percent) # How could I make it stop once % 100 in a print?
 
-    if percent > 100 and Loading == Full_capacity:
-        percent = 100
-        #Plumbing_op = False
-        Pumps = False
-        Valves = False
-        Throttles = False
-        print("Falcon 9 Fully Loaded with Propellant!")
-    
-    
+if percent > 90 and Full_capacity:
+    percent = 100
+    #Plumbing_op = False
+    Pumps = False
+    Valves = False
+    Throttles = False
+    print("Falcon 9 Fully Loaded with Propellant!")
+
+
 #for Loading in range(3000, Full_capacity): # What kind of step should I do to show exponential growth to achieve a natural loading sequence, instead of a linear 1% , 2%, 3%...
     #print("Loading Initialized Show increase in %...")
     #Loading += 300 # Show delta % in increments until it reaches Full capacity. # Propellant variable, must be added in delay. Let the load increase exponentially.
@@ -85,17 +87,13 @@ while percent < 95 and Loading < Full_capacity:
     #     print("Loading Completed")
     #     print("Proceed to Check 3")
 
-
-
-
-
 # 3 - LOX Chilldown - Falcon 9's Hardware is being adapted for cryogenic temperatures to maximize efficiency of LOX flow
-# and avoid boil off due to contact of a possible warm metal [Causing issues like thermal shock, cavitation and unstable flow].
+# and avoid boil off due to contact of warm metal/pipes/hardware [Causing issues like thermal shock, cavitation and unstable flow].
 
 
-# LOX_T = (-340 - 32)/ 1.8 # Found Fahrenheit, Converted to Celsius [C].
-# print(LOX_T)
-#RP_1_T =
+LOX_T = (-340 - 32)/ 1.8 # Found Fahrenheit, Converted to Celsius [C].
+print(LOX_T)
+RP_1_T = 2
 
 
 #LD Verifies Go for Launch.
@@ -106,9 +104,46 @@ while percent < 95 and Loading < Full_capacity:
 
 
 
+
+# 4 - 2nd Stage (S2) LOX load begins.
+
+
+
+
+# 5 - Engine Chill
+# 6 - Strongback retract
+# 7 - 1st Stage LOX Loaded
+# 8 - 2nd Stage LOX Loaded
+# 9 - F9 In Startup
+# 10 - TanK Press
+# 11 - LD Go for Launch
+
 # Launch Phase [#2] - Ignite and Punch through the Atmosphere.
+
+# 12 -  Engine Ignition # Time Delay for Countdown is crucial here.
+# 13 -  Liffoff
+# 14 -  Max-Q
+
 # Staging Phase [#3] - Throw away dead weight. [First Stage Booster - Heavyweight figther against extreme aerodynamic loads].
+
+
+# 15 - MECO
+# 16 - Stage Separation
+# 17 - SES-1  
+# 18 - Fairing Separation
+# 19 - 1st Stage Entry Burn
+# 20 - 1st Stage Entry Burn Ends
+
+
 # Orbital Phase [#4] - Upper Stage does the precision work.
+
+# 21 - 1st Stage Burn Begins
+# 22 - 1st Stage Landing  
+# 23 - 2nd Stage Engine Cutoff (SECO-1)
+# 24 - 2nd Stage Engine Start (SES-1)
+# 25 - 2nd Stage Engine Cutoff (SECO-2)
+# 26 - Starlink Deploy
+
 # Recovery Phase [#5] - Upper Stage Booster comes home.
 # Mission Phase [#6] - Satellites are released.
 
