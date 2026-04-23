6192dfd0a1119adf69059eb82f769e12d5defe7e
Borja Manuel Fernandez Escarraman <171446222+BorjaEscarraman@users.noreply.github.com>
2026-04-22 23:40:37 -0400

    Learning Dictionaries w/ One Piece.

---
+++ Python Theory/Training3.py
@@ -0,0 +1,22 @@
+# Lesson --> Learning Dictionaries w/ One piece.
+
+#String Variable are Devil Fruit Users.
+captain = "Luffy"  #It gives only 1 box = 1 fact. Luffy Is the captain.
+
+# Dictionaries is like the entire Straw Hat Crew Profile Card.
+
+zoro = {
+    "name": "Zoro",
+    "role": "Luffy's Second Hand",
+    "Combat Style": "Swordsman"
+    "Bounty": 1_111_000_000
+}
+
+print(zoro["name"])
+
+
+
+
+# But wait, how are Dictionaries different from Lists? they're roles are similar. 1 box = Multiple Items.
+
+
