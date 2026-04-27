48ce4d44257d76ff454fbc5247b3ce8530d386ec
Borja Manuel Fernandez Escarraman <171446222+BorjaEscarraman@users.noreply.github.com>
2026-04-26 22:26:10 -0400

    Randomizer to call out the keys of dictionaries.

--- Python Theory/Training5.py
+++ Python Theory/Training5.py
@@ -7,10 +7,15 @@
     # Function (5), machine that conserves physics / equations for reusability.
     # Analogy --> Demon Slayer.
 
-# Context... Tanjiro fights Muzan. I want them to throw attacks to each other. Make them dodge erratically [dodge position driven by random indices within a range.],
+# Context... Tanjiro fights Muzan. 
+
+# I want them to throw attacks to each other. Make them dodge erratically [dodge position driven by random indices within a range.],
 # and develop new abilities during the combat. 
 
 # Disclaimer... I do not read the manga, so I wont be able to define Muzan's nor Tanjiro's Strongest Combat Level, so I'll go with what the anime showcased so far.
+
+import random # built-in random module imported for randomized calls on techniques, dodging positions, Strength stages, 
+#environments [To account for the Tambourine Demon that helps muzan change surroundings]
 
 # Define Tanjiro and Muzan's Fighting Styles with Dictionaries...
 Tanjiro = { # How much damage each technique causes?
@@ -18,21 +23,21 @@ Tanjiro = { # How much damage each technique causes?
          # Could I define a list within a key in the dictionary? Yes!.... Corrected, Initially I used a list,
          # But I wanted to attach a damage count [Not an index, which is a mathematical position number to call values in a list.] 
          # to the style, so I "nested the dictionary" to have double keys. NEW CONCEPT].
-        "First Form, Water Surface Slash": 2500, # Damage.
-         "Second Form, Water Wheel": 3000,
-         "Second Form, Lateral Water Wheel": 3500,
-         "Third Form, Flowing Dance": 4000,
-         "Fourth Form, Striking Tide": 4500,
-         "Fourth Form, Striking Tide, Turbulent": 5000,
-         "Fifth Form, Blessed Rain After the Drought": 5500,
-         "Sixth Form: Whirlpool": 6000,
-         "Seventh Form: Drop Ripple Thrust": 6500,
-         "Eighth Form: Waterfall Basin": 7000,
-         "Ninth Form: Splashing Water Flow": 7500,
-         "Tenth Form: Constant Flux": 8000
+        "First Form, Water Surface Slash!": 2500, # Damage.
+         "Second Form, Water Wheel!": 3000,
+         "Second Form, Lateral Water Wheel!": 3500,
+         "Third Form, Flowing Dance!": 4000,
+         "Fourth Form, Striking Tide!": 4500,
+         "Fourth Form, Striking Tide, Turbulent!": 5000,
+         "Fifth Form, Blessed Rain After the Drought!": 5500,
+         "Sixth Form: Whirlpool!": 6000,
+         "Seventh Form: Drop Ripple Thrust!": 6500,
+         "Eighth Form: Waterfall Basin!": 7000,
+         "Ninth Form: Splashing Water Flow!": 7500,
+         "Tenth Form: Constant Flux!": 8000
     } , # Commas must be after curly bracket to follow up with the outer dictionary, and avoid syntax error.
 
-    "Hinokami Kagura": 10_500, # Sun Breathing, his innate fighting form.
+    "Hinokami Kagura!": 10_500, # Sun Breathing, his innate fighting form.
     "Demon Slayer Mark lvl 1": 3000, # The red mark in his face. Upgrade lvl's by 3x.
     "Demon Slayer Mark lvl 2": 9000, # 
     "Demon Slayer Mark lvl 3": 27_000,
@@ -40,11 +45,27 @@ Tanjiro = { # How much damage each technique causes?
     "Raw_sword_style": 900
 }
 
-print(Tanjiro["Water Breathing"]["Second Form, Water Wheel"]) # To access a key I must use squared brackets... since it's nested, double that. [inner Dictionary.]
-print(Tanjiro["Hinokami Kagura"]) # Outer Dictionary.
+# print(Tanjiro["Water Breathing"]["Second Form, Water Wheel"]) # To access a key I must use squared brackets... since it's nested, double that. [inner Dictionary.]
+# print(Tanjiro["Hinokami Kagura"]) # Outer Dictionary.
 # Both Inner and Outer dictionaries work properly, Cool!
 
+random_technique = random.choice(list(Tanjiro["Water Breathing"])) # Random.choice, picks a random item from my dictionary, after converting keys into a list.
+#This is the randomizer I'll need to call the Techniques during the fight.
+# form = random.choice(list(Tanjiro["Water Breathing"]))
+# print(form)
+
+print(random_technique)
+
+# random_technique_damage = Tanjiro[random_technique]
+
+# print(random_technique,"=", random_technique_damage) 
+
+# How could I call nested dictionary's keys?
+
+
+
 Invisible_world = False # Defined Tanjiro's secret ability "Invisible World" learned in the Infinity Castle Part 1 to Maximize his dodging / fighting capability.
+
 
 Muzan = { # Since I am unaware of how strong Muzan is and his abilities, I'll define his layers to be defeated.
     "Shapeshifting": "Camouflaged",
@@ -65,17 +86,21 @@ Muzan = { # Since I am unaware of how strong Muzan is and his abilities, I'll de
 
 print(Muzan["Absorption"]["Akaza"]["HP"])
 
-# Tatakae Time!
-# note. Make Muzan Immune to some of Tanjiro's Techniques depending on which layer we are. On that account, Tanjiro
-# note 2. Tanjiro will have to develop a new technique in the middle of the fight, to give him a better chance against the Invincible & Nefarious Muzan.
+# Tatakae!
+# note. Make Muzan Immune to some of Tanjiro's Techniques depending on which layer we are.
+# note 2. Tanjiro will have to develop a new technique in the middle of the fight, to stand a chance against the Invincible & Nefarious Muzan.
 
-# How can I call dictionary key within a loop or funtion?
-import random
+# How can I call dictionary keys within a loop? To summon the randomized_keys during the fight.
+# for form in Tanjiro.items():
+#     print(form)
 
-Tanjiro_dodging = list(range(1,101))
 
-for dodge in range(Tanjiro_dodging):
-    print(random.randint[dodge])
+# Tanjiro_dodging = list(range(1,101)) # Dodging Positions.
+
+# for dodge in range(len(Tanjiro_dodging)):
+#     print(random.randint[dodge])
+
+
 
 
 # def Combat (Tanjiro, Muzan):
